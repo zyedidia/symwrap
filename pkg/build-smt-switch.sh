18fa0a34ee2cad09649cc5e1e2007196f24f39af
Zachary Yedidia <zyedidia@gmail.com>
2022-10-08 13:33:37 -0700

    Initial commit

---
+++ pkg/build-smt-switch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Usage: ./build-smt-switch <solver>
+
+SOLVER=$1
+
+cd smt-switch
+./contrib/setup-$SOLVER.sh
+./configure.sh --$SOLVER
+cd build
+make
+
+cd ../../
+
+mkdir lib
+cp smt-switch/build/libsmt-switch.so ./lib
+cp smt-switch/build/$SOLVER/libsmt-switch-$SOLVER.so ./lib
